f5ead3005bc59d5ed98800e1953e8c42d5e880ce
ericgaspar <junk.eg@free.fr>
2022-05-22 11:38:11 +0200

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -1,6 +1,1 @@
 $DefaultPasswords['admin'] = crypt(__PASSWORD__);
-
-#$DefaultPasswords['edit'] = crypt('edit_password');
-#$DefaultPasswords['read'] = array(crypt('alpha'), crypt('beta'));
-#$DefaultPasswords['edit'] = crypt('beta');
-#$DefaultPasswords['upload'] = crypt('beta');
